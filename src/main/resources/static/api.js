60bfacd49c9a567014e79aa427a350bd35aaa3b3
Diego Flores Chavarria <dfloresc1602@alumno.ipn.mx>
2022-06-04 22:46:27 -0500

    api.js: request al controller realizada

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -17,3 +17,24 @@ async function crearReservacion(){
       console.log(content);
 
 }
+
+async function crearPedido(){
+    let datos = {};
+    datos.name = document.getElementById("nombre").value;
+    datos.email = document.getElementById("email").value;
+    datos.product = document.getElementById("producto").value;
+    datos.cellphone = document.getElementById("telefono").value;
+    datos.quantity = document.getElementById("cantidad").value;
+    datos.address = document.getElementById("direccion").value;
+    console.log(datos);
+    const request = await fetch('/cafeteria/pedido', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+      });
+      const content = await rawResponse.json();
+      console.log(content);
+}
